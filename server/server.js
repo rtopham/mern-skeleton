f3840810304b6f69e862446b48b96034e38b0229
Reed Topham <reedweb@tophamonline.com>
2018-10-11 03:44:51 +0000

    Added Mongoose/Mongodb Connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import config from '../config/config'
 import app from './express'
+import mongoose from 'mongoose'
 
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
   });
+
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true })
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
+})
 
 app.listen(config.port, (err) => {
     if (err) {
